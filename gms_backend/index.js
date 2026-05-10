eefa81434fceac28670b0a8ec4a5bbc868c6a615
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-05-11 00:02:22 +0530

    created products files

--- gms_backend/index.js
+++ gms_backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import db from './config.js';
+import productRoutes from './routers/productRoutes.js';
+import userRoutes from './routers/userRouter.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000- test branch protection");
---
+++ gms_backend/routers/productRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {createProduct, getProducts, getProductById, updateProduct} from '../controllers/productController.js';
+
+const router = express.Router();
+
+app.use(express.json());
+
+router.post('/', createProduct);
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.put('/:id', updateProduct);
+
+export default router;
+
