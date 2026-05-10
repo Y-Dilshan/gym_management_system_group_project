8fd4a9350ee6b4b36e1e1b317fe3b3518ac09b47
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-05-11 00:38:06 +0530

    resolved problems

--- gms_backend/routes/userRoutes.js
+++ gms_backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import {createUser, getUsers, getUserById, updateUser} from '../controllers/userController.js'; 
+import {createUser, getUsers, getUserById, updateUser} from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.post('/', createUser);
-router.get('/', getUsers);  
+router.post('/create', createUser);
+router.get ('/', getUsers);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 
