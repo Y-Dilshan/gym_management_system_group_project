0159751e60f413ffbcd3433cb587c74a85e3d9bd
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-05-29 00:40:57 +0530

    added icons

--- gms_frontend/src/pages/adminDashboard.jsx
+++ gms_frontend/src/pages/adminDashboard.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
+import { LuUsers } from "react-icons/lu";
+import { FaClipboardList } from "react-icons/fa";
+import { LiaThListSolid } from "react-icons/lia";
+import { MdPayments } from "react-icons/md";
 
 export default function AdminDashboard(){
     return(
         <div className="w-full h-full flex items-center justify-center">
             <div className = "w-[20%] h-screen bg-[#333333] flex flex-col items-center justify-start py-10">
-                <h2 className="text-xl font-bold mb-4">Navigation</h2>
+                <img src="/logo.png" alt="Logo" className="w-[150px] h-[150px] mb-4 flex" />
+                <h2 className = "flex text-[#D4AF37] text-4xl">Admin Dashboard</h2>
                 <ul>
-                    <li className="mb-2"><Link to="/admin/dashboard" className="text-blue-500 hover:underline">Dashboard</Link></li>
-                    <li className="mb-2"><Link to="/admin/users" className="text-blue-500 hover:underline">Users</Link></li>
-                    <li className="mb-2"><Link to="/admin/settings" className="text-blue-500 hover:underline">Settings</Link></li>
+                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/dashboard" className="text-white hover:text-4xl flex gap-2"><FaClipboardList />Products</Link></li>
+                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/users" className="text-white hover:text-4xl flex gap-2"><LuUsers /> Users</Link></li>
+                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/settings" className="text-white hover:text-4xl flex gap-2"><LiaThListSolid /> Orders</Link></li>
+                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/settings" className="text-white hover:text-4xl flex gap-2"><MdPayments />Payments</Link></li>
                 </ul>
             </div>
             
