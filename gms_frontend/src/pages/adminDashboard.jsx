22ef4de81b79c768390336d616e0dbc8c73ee79b
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-05-29 01:47:51 +0530

    started to create admin add product page

--- gms_frontend/src/pages/adminDashboard.jsx
+++ gms_frontend/src/pages/adminDashboard.jsx
@@ -9,17 +9,16 @@ export default function AdminDashboard(){
         <div className="w-full h-full flex items-center justify-center">
             <div className = "w-[20%] h-screen bg-[#333333] flex flex-col items-center justify-start py-10">
                 <img src="/logo.png" alt="Logo" className="w-[150px] h-[150px] mb-4 flex" />
-                <h2 className = "flex text-[#D4AF37] text-4xl">Admin Dashboard</h2>
+                <h2 className = "flex text-[#D4AF37] text-4xl text-bold">Admin Dashboard</h2>
                 <ul>
                     <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/dashboard" className="text-white hover:text-4xl flex gap-2"><FaClipboardList />Products</Link></li>
                     <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/users" className="text-white hover:text-4xl flex gap-2"><LuUsers /> Users</Link></li>
-                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/settings" className="text-white hover:text-4xl flex gap-2"><LiaThListSolid /> Orders</Link></li>
-                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/settings" className="text-white hover:text-4xl flex gap-2"><MdPayments />Payments</Link></li>
+                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/orders" className="text-white hover:text-4xl flex gap-2"><LiaThListSolid /> Orders</Link></li>
+                    <li className="mb-2 text-3xl pt-[20px]"><Link to="/admin/payments" className="text-white hover:text-4xl flex gap-2"><MdPayments />Payments</Link></li>
                 </ul>
             </div>
             
             <div className="w-[80%] h-screen flex items-center justify-center">
-                <button className = "bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 ">+</button>
                 <Link to="/admin/add-product" className="ml-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">Add Product</Link>
             </div>
         </div>
