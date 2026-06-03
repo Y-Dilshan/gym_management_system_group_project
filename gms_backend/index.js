9294b64279859b506b2ce76e66c22737ba245336
janith-sasindu <sasindumihiran75@gmail.com>
2026-06-04 00:28:14 +0530

    signup connected to backend

--- gms_backend/index.js
+++ gms_backend/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // ← serves profile pictures
 
+app.use(cors());
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
