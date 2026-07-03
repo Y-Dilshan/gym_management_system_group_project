98aa78938d1c5d999ce48a62e75d8fcbe79d6158
janith-sasindu <sasindumihiran75@gmail.com>
2026-07-03 15:39:15 +0530

    change backend

--- gms_backend/index.js
+++ gms_backend/index.js
@@ -19,6 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // ← serves profile pictures
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads"))); // ← serves profile pictures
 
 app.use(cors());
 
